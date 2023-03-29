2cfcb5bff0d15e1a1aea62ba159e64534231b5f2
cbreland <53184887+cbreland@users.noreply.github.com>
2023-03-29 16:11:23 -0400

    Update parse.py

--- county/parse.py
+++ county/parse.py
@@ -21,15 +21,15 @@ def parse_case_data(item: CaseItem, spider: CaseScraper) -> PackedCase:
     case_number = item['case_number']
     link = item['link']
     county = item['county']
-
-# -------------------------------------------------------------------------------------
-
+    
+    """ -------------------------------------------------------------------------------------
+    
     #
     # (STEP 1) THE FOLLOWING STEPS ARE BASED ON GETTING CASE AND PARTY DATA FROM DOCKET ENTRIES
     #
     # Create a dictionary of initial case data as key, value pairs
     # In the following data structure
-    """
+    
     case_dict
         file_date: datestring format '01/01/1900 
         case_status_date: datestring 'start with filed_date'
@@ -40,15 +40,15 @@ def parse_case_data(item: CaseItem, spider: CaseScraper) -> PackedCase:
         case_type: str
     """
     case_dict = parse_case_related_data(soup, county)
-
-# -------------------------------------------------------------------------------------
+    
+    """ -------------------------------------------------------------------------------------
 
     #
     # (STEP 2)
     #
     # Create a list of dictionaries containing the docket entry data as key, value pairs
     # In the following data structure
-    """
+    
     docket_dicts
         entry: str
         date_time: datestring format '01/01/1900 
@@ -56,14 +56,14 @@ def parse_case_data(item: CaseItem, spider: CaseScraper) -> PackedCase:
     """
     docket_dicts = parse_docket_entries(soup)
 
-# -------------------------------------------------------------------------------------
+    """ -------------------------------------------------------------------------------------
 
     #
     # (STEP 3)
     #
     # Create a list of dictionaries with defendant data as key, value pairs
     # In the following data structure
-    """
+   
     defendant_dicts
         defendant: str (defendant name)
         total_address: str (combined address)
@@ -77,7 +77,7 @@ def parse_case_data(item: CaseItem, spider: CaseScraper) -> PackedCase:
     """
     defendant_dicts, plaintiffs = parse_plaintiffs_and_defendants(soup, link)
 
-# -------------------------------------------------------------------------------------
+    """ -------------------------------------------------------------------------------------
 
     #
     # (STEP 4)
@@ -87,7 +87,7 @@ def parse_case_data(item: CaseItem, spider: CaseScraper) -> PackedCase:
     #
     # Return a tuple with each dictionary included
     # In the following data structure
-    """
+   
     case_docket_data: dict
         hearing_date: datestring format '01/01/1900 
         case_status: str
@@ -109,42 +109,45 @@ def parse_case_data(item: CaseItem, spider: CaseScraper) -> PackedCase:
         agreement_date: datestring format '01/01/1900 
         is_bankruptcy_filed: bool
     """
-
-    # Parse CaseParty and Case fields from docket
     case_docket_data, case_party_docket_data = parse_docket_fields(
         docket_dicts, case_dict, plaintiffs, case_number
     )
 
-# -------------------------------------------------------------------------------------
+    """ -------------------------------------------------------------------------------------
 
     #
     # STEPS (5 - 8) PACKAGING DATA INTO DATACLASSES
     #
-
+    """
+    
+    """
     #
     # (STEP 5)
     #
     # Create a list of CaseDocket objects
+    """
     dockets = [
         CaseDocket(**docket_entry)
         for docket_entry in docket_dicts
     ]
 
-# -------------------------------------------------------------------------------------
+    """ -------------------------------------------------------------------------------------
 
     #
     # (STEP 6)
     #
     # Create a Case object
+    """
     case = Case(**case_docket_data)
-
-# -------------------------------------------------------------------------------------
+    
+    """ -------------------------------------------------------------------------------------
 
     #
     # (STEP 7)
     #
     # Create a list of CaseParty objects with dictionaries that were created
     # Also add the list of CaseDockets as a dictionary.
+    """
     case_parties = [
         CaseParty(
             **case_party_docket_data,
@@ -154,10 +157,11 @@ def parse_case_data(item: CaseItem, spider: CaseScraper) -> PackedCase:
         for defendant_dict in defendant_dicts
     ]
 
-# -------------------------------------------------------------------------------------
+    """ -------------------------------------------------------------------------------------
 
     #
     # (STEP 8)
     #
     # Create BaseCasePacked object with the list of Case and list of CaseParty objects
+    """
     return PackedCase(case, case_parties)
